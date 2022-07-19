0ae94a521f1bd1273375de37381da042e1cb5fd6
Toygoon <toygoon@gmail.com>
2022-07-19 16:22:45 +0900

    Edit; Changing runner scripts

--- 2.2StartLoader/qemu/devenv/run_qemu.sh
+++ 2.2StartLoader/qemu/devenv/run_qemu.sh
@@ -32,5 +32,14 @@ sudo umount $MOUNT_POINT
 # Maximum memory is 1G
 # Set UEFI Environments
 # Set mount drive points
-readonly=on
-qemu-system-x86_64 -m 1G -drive if=pflash,format=raw,readonly,file=$DEVENV_DIR/OVMF_CODE.fd -drive if=pflash,format=raw,file=$DEVENV_DIR/OVMF_VARS.fd -drive if=ide,index=0,media=disk,format=raw,file=$DISK_IMG
+
+#readonly=on
+#qemu-system-x86_64 -m 1G -drive if=pflash,format=raw,readonly,file=$DEVENV_DIR/OVMF_CODE.fd -drive if=pflash,format=raw,file=$DEVENV_DIR/OVMF_VARS.fd -drive if=ide,index=0,media=disk,format=raw,file=$DISK_IMG
+
+qemu-system-x86_64 \
+    -m 1G \
+    -drive if=pflash,format=raw,readonly,file=$DEVENV_DIR/OVMF_CODE.fd \
+    -drive if=pflash,format=raw,file=$DEVENV_DIR/OVMF_VARS.fd \
+    -drive if=ide,index=0,media=disk,format=raw,file=$DISK_IMG \
+    -monitor stdio \
+    $QEMU_OPTS
